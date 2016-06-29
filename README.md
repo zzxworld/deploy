cc7738527a744c57fc348b3ce47ad93231bda6f8
zzxworld <zzxworld@gmail.com>
2016-06-29 18:35:35 +0800

    refactor rbenv install script.

--- README.md
+++ README.md
@@ -2,5 +2,18 @@
 
 常用的开发工具部署脚本。
 
-测试平台:
+## 测试平台:
+
 * CentOS 7
+
+## 使用:
+
+### rbenv
+```
+curl https://git.io/vobMw | bash
+```
+
+使用 _Ruby China_ 镜像:
+```
+curl https://git.io/vobMw | bash -s https://gems.ruby-china.org
+```
--- rbenv.sh
+++ rbenv.sh
@@ -1,23 +1,79 @@
 #!/bin/bash
 
-RB_VERSION=2.3.1
-CN_MIRROR=https://gems.ruby-china.org/
+RUBY_VERSION=2.3.1
+MIRROR=$1
+
+
+function install_plugin_china_mirror() {
+    if [[ ! -z "${MIRROR}" ]]; then
+        WORD="ruby-china.org"
+        echo "use mirror"
+        if test "${MIRROR#*${WORD}}" != "${MIRROR}"
+        then
+            if [[ ! -d ~/.rbenv/plugins/rbenv-china-mirror ]]; then
+                git clone https://github.com/andorchen/rbenv-china-mirror.git --depth=1 ~/.rbenv/plugins/rbenv-china-mirror
+            fi
+        fi
+    fi
+}
+
+function install_ruby() {
+    if [[ -f ~/.rbenv/bin/rbenv ]]; then
+        ~/.rbenv/bin/rbenv install -v ${RUBY_VERSION}
+        ~/.rbenv/bin/rbenv global ${RUBY_VERSION}
+    fi
+}
+
+function install_gem() {
+    if [[ -f ~/.rbenv/shims/ruby ]]; then
+        git clone https://github.com/rubygems/rubygems.git --depth=1
+        if [[ -d rubygems ]]; then
+            cd rubygems
+            ~/.rbenv/shims/ruby setup.rb
+        fi
+    fi
+
+    if [[ ! -z "${MIRROR}" ]]; then
+        if [[ -f ~/.rbenv/shims/gem ]]; then
+            ~/.rbenv/shims/gem sources --add ${MIRROR}/ --remove https://rubygems.org/
+        fi
+    fi
+}
+
+function install_bundle() {
+    if [[ -f ~/.rbenv/shims/gem ]]; then
+        ~/.rbenv/shims/gem install bundle
+    fi
+
+    if [[ ! -z "${MIRROR}" ]]; then
+        if [[ -f ~/.rbenv/shims/bundle ]]; then
+            ~/.rbenv/shims/bundle config mirror.https://rubygems.org ${MIRROR}
+        fi
+    fi
+}
 
 function do_init() {
-    sudo yum install gcc -y
+    sudo yum install gcc openssl-devel readline-devel zlib-devel gdbm-devel -y
 }
 
 function do_install() {
     if [[ ! -d ~/.rbenv ]]; then
-        git clone git://github.com/sstephenson/rbenv.git .rbenv
+        git clone git://github.com/sstephenson/rbenv.git --depth=1 ~/.rbenv
     fi
 
     if [[ ! -d ~/.rbenv/plugins/ruby-build ]]; then
-        git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+        git clone git://github.com/sstephenson/ruby-build.git --depth=1 ~/.rbenv/plugins/ruby-build
     fi
 
-    if [[ ! -d ~/.rbenv/plugins/ruby-china-mirror ]]; then
-        git clone https://github.com/andorchen/rbenv-china-mirror.git ~/.rbenv/plugins/rbenv-china-mirror
+    install_plugin_china_mirror
+    install_ruby
+    install_gem
+    install_bundle
+}
+
+function do_config() {
+    if [[ ! -d ~/.rbenv/plugins/rbenv-vars ]]; then
+        git clone https://github.com/rbenv/rbenv-vars.git --depth=1 ~/.rbenv/plugins/rbenv-vars
     fi
 
     if [[ ! -f ~/.gemrc ]]; then
@@ -26,7 +82,7 @@ function do_install() {
 
     if grep -Fq rbenv ~/.bash_profile
     then
-        echo 'WARING: rbenv is exists!'
+        echo 'WARING: rbenv is exists in bash_profile!'
     else
         echo '' >> ~/.bash_profile
         echo '# rbenv support start' >> ~/.bash_profile
@@ -36,15 +92,12 @@ function do_install() {
         echo '# rbenv support end' >> ~/.bash_profile
     fi
 
-    ~/.rbenv/bin/rbenv install -v ${RB_VERSION}
-    ~/.rbenv/bin/rbenv global ${RB_VERSION}
-}
-
-function do_config() {
-    echo 'install rbenv successful.'
+    source ~/.bash_profile
 }
 
 
 do_init
 do_install
 do_config
+
+echo 'install rbenv successful.'
