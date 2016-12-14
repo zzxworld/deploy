b4a24f7067f8445d77f7362028bb2a590292f2f6
zzxworld <zzxworld@gmail.com>
2016-12-14 12:06:57 +0800

    添加基于 vagrant 虚拟机的部署配置示例文件

---
+++ fabfile-vagrant.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from fabric.api import env, execute
+from tasks.nginx import *
+from tasks.mysql import *
+from tasks.redis import *
+from tasks.ruby import *
+from tasks.php import *
+
+env.hosts = ['127.0.0.1']
+env.port = 2222
+env.user = 'vagrant'
+env.password = 'vagrant'
+
+env.mysql_root_password = '123456'
+
+env.ruby_mirror = 'https://gems.ruby-china.org'
+env.rbuild_mirror = 'https://cache.ruby-china.org'
+
+env.php_mirror = 'http://cn2.php.net'
