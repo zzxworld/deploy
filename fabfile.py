a7fdda05313a7641bf33f48419ec2433c9832c95
zzxworld <zzxworld@gmail.com>
2016-12-14 12:06:15 +0800

    添加 fabric 主文件

---
+++ fabfile.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from tasks.system import *
+from tasks.nginx import *
+from tasks.mysql import *
+from tasks.redis import *
+from tasks.ruby import *
+from tasks.php import *
